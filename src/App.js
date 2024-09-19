99ba1442ce1c35c7b2f42bd371012e2598421cca
Sarah Albert <sgalbert818@yahoo.com>
2024-09-19 11:30:51 -0600

    testing deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Home from './pages/Home';
+import Home from './pages/Home.jsx';
 import { Amplify } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
